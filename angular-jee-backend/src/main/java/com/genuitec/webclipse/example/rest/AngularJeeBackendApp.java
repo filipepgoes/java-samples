5c5254f56a8a514c4c692bc9f772bb06d403347a
Filipe Goes <filipe.goes@previc.gov.br>
2020-05-05 13:34:48 -0300

    Adicionados angular.

---
+++ angular-jee-backend/src/main/java/com/genuitec/webclipse/example/rest/AngularJeeBackendApp.java
@@ -0,0 +1,8 @@
+package com.genuitec.webclipse.example.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("jaxrs")
+public class AngularJeeBackendApp extends Application {
+}
